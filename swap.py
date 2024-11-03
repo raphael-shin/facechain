ca4a085cd78a79f3106717fb349c550623507601
Shin, Jungseob <jungseob@amazon.com>
2024-11-04 01:59:56 +0900

    change numpy

--- swap.py
+++ swap.py
@@ -3,19 +3,19 @@ import argparse
 from modelscope.outputs import OutputKeys
 from modelscope.pipelines import pipeline
 
-def face_fusion(source_path, target_path, output_path):
+def face_fusion(user_path, template_path, output_path):
     image_face_fusion = pipeline('face_fusion_torch',
                                 model='damo/cv_unet_face_fusion_torch', 
                                 model_revision='v1.0.3')
                                 
-    result = image_face_fusion(dict(template=source_path, user=target_path))                            
+    result = image_face_fusion(dict(template=template_path, user=user_path))                            
     cv2.imwrite(output_path, result[OutputKeys.OUTPUT_IMG])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Face Fusion')
-    parser.add_argument('-s', '--source', required=True, help='source image path')
-    parser.add_argument('-t', '--target', required=True, help='target image path')
-    parser.add_argument('-o', '--output', required=True, help='output image path')
+    parser.add_argument('-u', '--user_path', required=True, help='user image path')
+    parser.add_argument('-t', '--template_path', required=True, help='template_path image path')
+    parser.add_argument('-o', '--output_path', required=True, help='output image path')
     
     args = parser.parse_args()
-    face_fusion(args.source, args.target, args.output)
+    face_fusion(args.user_path, args.template_path, args.output_path)
